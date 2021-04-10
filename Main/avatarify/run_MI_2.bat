7267e081dde98940109c13dbc65262b9864fdea2
Superception-Ginza <kasahara@csl.sony.co.jp>
2021-04-11 03:13:29 +0900

    double avatarify process. passed, but VRAM hit limit 8GB

--- Main/avatarify/run_MI_2.bat
+++ Main/avatarify/run_MI_2.bat
@@ -5,4 +5,4 @@ call scripts/settings_windows.bat
 set CONFIG=fomm/config/vox-adv-256.yaml
 
 set PYTHONPATH=%PYTHONPATH%;%CD%;%CD%/fomm
-call python afy/spout_osc_fomm.py --instance_id 2 --config %CONFIG% --relative --adapt_scale --no-pad --checkpoint vox-adv-cpk.pth.tar %*
+call python afy/ndi_spout_osc_fomm.py --instance_id 2 --config %CONFIG% --relative --adapt_scale --no-pad --checkpoint vox-adv-cpk.pth.tar %*
