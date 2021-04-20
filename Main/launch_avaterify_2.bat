20179f33eebd5c1489492e87a2fc34a616b827be
Superception-Ginza <kasahara@csl.sony.co.jp>
2021-04-21 03:22:29 +0900

    delete olds add some

---
+++ Main/launch_avaterify_2.bat
@@ -0,0 +1,5 @@
+@echo off
+
+cd avatarify
+call .py_azabu/Scripts/activate.bat
+call run_MI_2.bat
--- Main/setup_mp.bat
+++
@@ -1,5 +0,0 @@
-
-python -m venv .venv_td
-cd .venv_td/Scripts
-call activate.bat
-pip install mediapipe
