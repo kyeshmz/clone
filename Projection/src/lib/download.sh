3235eb8ca8c6743366ee5cb3b95874e83408cf70
kyeshmz <kyeshimizu@gmail.com>
2023-04-19 16:42:17 +0900

    add ping and iputils

--- Projection/src/lib/download.sh
+++ Projection/src/lib/download.sh
@@ -3,7 +3,7 @@
 
 # for stylegan2
 # this wget doesnt work
-wget "https://nvlabs-fi-cdn.nvidia.com/stylegan2/networks/stylegan2-ffhq-config-f.pkl -O ./stylegan2/networks/stylegan2-ffhq-config-f.pkl"
+wget "https://nvlabs-fi-cdn.nvidia.com/stylegan2/networks/stylegan2-ffhq-config-f.pkl" -O "./stylegan2/networks/stylegan2-ffhq-config-f.pkl"
 
 # for ps2p
 wget  "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1bMTNWkh5LArlaWSc_wa8VKyq2V42T2z0' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1bMTNWkh5LArlaWSc_wa8VKyq2V42T2z0" -O './ps2p/pretrained_models/psp_ffhq_encode.pt'
